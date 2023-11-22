29a4206618830103d4b7135b8c430e7f2e7a4789
kevintyj <hello@kevintyj.com>
2023-11-23 02:00:30 +0530

    feat: Add conventionalsets action to project

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,18 @@ import { verifyTitle } from './lint';
 
 type pullRequest = {
 	title: string
+	number: number
 };
 
 async function run(): Promise<void> {
 	const pullRequestPayload = github.context.payload.pull_request;
+
 	if (!pullRequestPayload?.title)
 		throw new Error('Pull Request or Title not found!');
+
 	const pullRequestObject: pullRequest = {
 		title: pullRequestPayload.title as string,
+		number: pullRequestPayload.number,
 	};
 
 	await verifyTitle(pullRequestObject.title, 'commitlint.config.js');
