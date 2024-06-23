39eaa3524082df552e7f1b3a483ea87a783fa702
kevintyj <hello@kevintyj.com>
2024-06-23 06:46:54 -0400

    feat: Force CJS using node runner

--- __tests__/log.test.ts
+++ __tests__/log.test.ts
@@ -5,4 +5,7 @@ describe('logger', () => {
 	it('log with title and error to return the correct string', () => {
 		expect(logWithTile('title', 'error')).toBe('title\n=====================\nerror');
 	});
+	it('log with title and empty content to throw error', () => {
+		expect(() => logWithTile('title', '')).toThrow('Content is empty');
+	});
 });
--- src/log.ts
+++ src/log.ts
@@ -1,5 +1,9 @@
 const SEPARATOR: string = '=====================';
+
 function logWithTile(title: string, content: string): string {
+	if (content === '')
+		throw new Error('Content is empty');
+
 	return `${title}\n${SEPARATOR}\n${content}`;
 }
 export default logWithTile;
