b803c648f17da0cb5ccd7d839c1532bf898b5b6a
kevintyj <hello@kevintyj.com>
2024-06-23 21:43:35 -0400

    feat: Add CJS plugin support

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { createRequire } from 'node:module';
 import * as github from '@actions/github';
 import * as core from '@actions/core';
 import handleError from './errHandle';
 import { verifyTitle } from './lint';
+
+/* eslint-disable no-restricted-globals */
+global.__filename = fileURLToPath(import.meta.url);
+global.__dirname = dirname(global.__filename);
+global.require = createRequire(import.meta.url);
+/* eslint-enable no-restricted-globals */
 
 type pullRequest = {
 	title: string
