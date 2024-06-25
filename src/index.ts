b846959812bd145ba127f78b842a6ceac1042409
kevintyj <hello@kevintyj.com>
2024-06-25 08:40:26 -0400

    feat: Build options

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { isAbsolute, resolve } from 'node:path';
-import { Module } from 'node:module';
 import * as github from '@actions/github';
 import handleError from './errHandle.js';
 import { verifyTitle } from './lint.js';
@@ -8,24 +6,6 @@ type pullRequest = {
 	title: string
 	number: number
 };
-
-/* eslint-disable ts/no-unsafe-return */
-const originalRequire = Module.prototype.require;
-
-// @ts-expect-error Module from node
-Module.prototype.require = function (request: string) {
-	if (!isAbsolute(request) && !request.startsWith('.')) {
-		const distNodeModulesPath = resolve(__dirname, 'node_modules', request);
-		try {
-			return originalRequire.call(this, distNodeModulesPath);
-		}
-		catch (err) {
-			// If it fails, fallback to the original behavior
-		}
-	}
-	return originalRequire.call(this, request);
-};
-/* eslint-enable ts/no-unsafe-return */
 
 async function run(): Promise<void> {
 	const pullRequestPayload = github.context.payload.pull_request;
